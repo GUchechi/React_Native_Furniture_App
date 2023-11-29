e531900022f879c82aa62aaa9ad4221b8a36634e
GUchechi <godswilluchendu7@gmail.com>
2023-11-29 05:18:58 +0100

    Navigation

---
+++ screens/Search.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, safeAreaView } from 'react-native'
+import React from 'react'
+
+const Search = () => {
+  return (
+    <safeAreaView>
+      <Text>Search</Text>
+    </safeAreaView>
+  )
+}
+
+export default Search
+
+const styles = StyleSheet.create({})
