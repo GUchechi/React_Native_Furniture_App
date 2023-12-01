b1c8ee8c4369cd9f060258ded6ed33613185c621
GUchechi <godswilluchendu7@gmail.com>
2023-12-01 17:10:23 +0100

    productCardView

--- components/products/ProductRow.jsx
+++ components/products/ProductRow.jsx
@@ -4,6 +4,7 @@ import styles from "./ProductRow.styles";
 import { Text } from "react-native";
 import { SIZES } from "../../constants";
 import { View } from "react-native";
+import ProductViewCard from "./ProductViewCard";
 
 export default function ProductRow() {
   const products = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -12,7 +13,7 @@ export default function ProductRow() {
     <View style={{ marginTop: SIZES.medium }}>
       <FlatList
         data={products}
-        renderItem={({ item }) => <Text>products</Text>}
+        renderItem={({ item }) => <ProductViewCard />}
         horizontal
         contentContainerStyle={{ columnGap: SIZES.medium }}
       />
---
+++ components/products/ProductViewCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import styles from './ProductViewCard.styles'
+
+export default function ProductViewCard() {
+  return (
+    <View>
+      <Text>ProductViewCard</Text>
+    </View>
+  )
+}
