7aab098492a05fbe7b88778dd98bb9412d2e413e
GUchechi <godswilluchendu7@gmail.com>
2023-12-01 12:52:00 +0100

    welcomePage

--- components/home/Welcome.styles.js
+++ components/home/Welcome.styles.js
@@ -11,6 +11,40 @@ const styles = StyleSheet.create({
     marginTop: top,
     color: color,
   }),
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.medium,
+    marginVertical: SIZES.medium,
+    height: 50,
+    marginHorizontal: SIZES.small
+  },
+  searchIcon: {
+    marginHorizontal: 10,
+    color: COLORS.gray,
+    marginTop: SIZES.small,
+  },
+  searchWrapper: {
+    flex: 1,
+    marginRight: SIZES.small,
+    borderRadius: SIZES.small,
+  },
+  searchInput: {
+    fontFamily: "regular",
+    width: "100%",
+    height: "100%",
+    paddingHorizontal: SIZES.small,
+  },
+  searchBtn: {
+    width: 50,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.primary,
+    borderRadius: SIZES.medium,
+  },
 });
 
 export default styles;
--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StyleSheet, Text,TextInput, View,TouchableOpacity, SafeAreaView } from "react-native";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { COLORS, SIZES } from "../constants";
 
 const Search = () => {
   return (
     <SafeAreaView>
-      <Text>Search</Text>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity>
+          <Feather name="search" size={24} style={styles.searchIcon} />
+        </TouchableOpacity>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onPressIn={() => {
+              navigation.navigate("Search");
+            }}
+            placeholder="What are you looking for?"
+          />
+        </View>
+
+        <View>
+          <TouchableOpacity style={styles.searchBtn}>
+            <Ionicons
+              name="camera-outline"
+              size={SIZES.xLarge}
+              color={COLORS.offwhite}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
