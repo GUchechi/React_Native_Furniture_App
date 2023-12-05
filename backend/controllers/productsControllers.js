102b77a65f7f0cb6d3cde759978ea0cfce01ac7d
GUchechi <godswilluchendu7@gmail.com>
2023-12-05 11:34:40 +0100

    added get Products and get Product

--- backend/controllers/productsControllers.js
+++ backend/controllers/productsControllers.js
@@ -1,21 +1,34 @@
 const Product = require("../models/Products");
 
 module.exports = {
+  // Create Product
   createProduct: async (req, res) => {
     const newProduct = new Product(req.body);
     try {
       await newProduct.save();
       res.status(200).json("Product created successfully");
     } catch (error) {
-      res.status(500).json("failed to create the product");
+      res.status(500).json("Failed to create the products");
     }
   },
 
-  getAllProduct: async(req, res) => {
+  //Get All Products
+  getAllProduct: async (req, res) => {
     try {
-        
+      const products = await Product.find().sort({ createdAt: -1 });
+      res.status(200).json(products);
     } catch (error) {
-        
+      res.status(500).json("Failed to get the products");
     }
-  }
+  },
+
+  // Get Individual Product
+  getProduct: async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json("Failed to get the product");
+    }
+  },
 };
